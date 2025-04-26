e643db006ebdc66e1ca31f6b0ab52fccdad2a337
JOO-gil <golbanglee7@gmail.com>
2025-04-26 08:36:18 +0000

    Flask-Practice-(connected HTML)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -21,44 +21,15 @@ def contact():
 #For form page (using HTML in here)
 @app.route('/form')
 def form():
-  return '''
-  <html>
-  <head>
-    <style>
-      body {
-          text-align: center;
-          font-family: Arial, sans-serif;
-          margin-top: 50px;
-      }
-      input[type="text"] {
-          padding: 10px;
-          font-size: 1rem;
-          width:200px;
-          margin-bottom: 20px;
-      }
-      button {
-          padding: 10px 20px;
-          font-size: 1rem;
-          margin-bottom: 20px;
-      }
-      form {
-          display: inline-block;
-      }
-    </style>
-  </head>
-  <body>
-    <form method="POST" action="/submit">
-    <input type="text" name="username" placeholder="Enter your name"><br>
-    <button type="submit">Submit</button>
-    </form>
-  </body>
-  </html>
-  '''
+  return render_template('form.html')
+  
 #For handling form submission
 @app.route('/submit', methods=['POST'])
 def submit():
+  
   # Retrieve the submitted username from the form
   name = request.form['username']
+  
   # Display a greeting message using the submitted username
   return f'<h2>Hello, {name}!</h2>'
 
