8b25f1fc35904b1665f1341bac0a7f000ecacf2d
JOO-gil <golbanglee7@gmail.com>
2025-04-28 11:33:48 +0000

    basic flask MVP(2025/04/28)
    flask server set up practice

--- main.py
+++ main.py
@@ -1,38 +1,19 @@
-from flask import Flask, request, render_template
-
-# Initialize the Flask application
-app = Flask(__name__)
-
-#For home page
-@app.route('/')
-def home():
-  return 'Hello, World!'
-
-#For about page
-@app.route('/about')
-def about():
-  return 'This is About page.'
-
-#For contact page
-@app.route('/contact')
-def contact():
-  return 'This is the Contact page.'
-
-#For form page (using HTML in here)
-@app.route('/form')
-def form():
-  return render_template('form.html')
-  
-#For handling form submission
-@app.route('/submit', methods=['POST'])
-def submit():
-  
-  # Retrieve the submitted username from the form
-  name = request.form['username']
-  
-  # Display a greeting message using the submitted username
-  return f'<h2>Hello, {name}!</h2>'
-
-# Start the Flask server
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3000)
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Form Page</title>
+  </head>
+  <body>
+    <h1>Welcom to the Form!</h1>
+    <form action="/submit" method="POST">
+      <div>
+        <label for="username">Enter your name:</label>
+        <input type="text" id="username" name="username" required />
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  </body>
+</html>
